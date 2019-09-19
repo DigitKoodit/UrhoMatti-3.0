db24b17d8895c8e42ce75615809ab3e180d546a0
PetroSilenius <pvsile@utu.fi>
2019-09-19 15:34:49 +0300

    Add landing page and logos

---
+++ src/views/Landing/LandingPage.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class LandingPage extends Component {
+
+  render() {
+    return (
+		<div className="landing-page page">
+			<h2 className="landing-heading">Urho Matti v3.0</h2>
+			<div className="landing-images">
+				<a href="https://www.digit.fi" target="_blank">
+					<img
+						className="landing-image"
+						src={require('../../images/digit.svg')}
+						alt="Digit ry"
+					/>
+				</a>
+				<a href="https://www.nucleus.fi" target="_blank">
+					<img
+						className="landing-image"
+						src={require('../../images/nucleus.png')}
+						alt="Nucleus ry"
+					/>
+				</a>
+			</div>
+		</div>
+	);
+  }
+}
+
+export default LandingPage;
