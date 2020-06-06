c9e7f4e88ed3bb381a1c2d26bbef510314ae9491
Petro Silenius <pvsile@utu.fi>
2020-06-06 10:05:29 +0300

    Change category by swiping on the category page

--- src/views/Category/Category.jsx
+++ src/views/Category/Category.jsx
@@ -1,41 +1,74 @@
 import React, { Component } from 'react';
 import MenuSongs from '../../components/MenuSongs';
+import db from '../../db.json';
 
-class Song extends Component {
+class Category extends Component {
+  render() {
+    const category = this.props.category;
+    let touchstartX = 0;
+    let touchendX = 0;
+    const gestureZone = document.getElementById('root');
 
-	render() {
-		var category = this.props.category;
-		return (
-			<>
-				<div className="page song-page">
-					<p className="category-header">
-						{category.id}. {category.title}{category.title.includes("40")&&" %"}
-					</p>
-					<div className="category-image-container">
-						<img
-							src={require('../../images/categories/' +
-								category.image +
-								'.png')}
-							alt={category.image}
-							height="100%"
-						/>
-					</div>
-					<MenuSongs
-						category={category.title}
-						songs={this.props.songs}
-					/>
-				</div>
-				<div className="song-footer">
-					<a
-						href={category.title}
-						className="song-footer-category"
-					>
-						{category.title}{category.title.includes("40")&&" %"}
-					</a>
-				</div>
-			</>
-		);
-	}
+    gestureZone.addEventListener(
+      'touchstart',
+      function (event) {
+        touchstartX = event.changedTouches[0].screenX;
+      },
+      false
+    );
+
+    gestureZone.addEventListener(
+      'touchend',
+      function (event) {
+        touchendX = event.changedTouches[0].screenX;
+        handleGesture();
+      },
+      false
+    );
+
+    function handleGesture() {
+      if (touchstartX - touchendX > 50) {
+        const categoryIndex = db['categories'].indexOf(category);
+        const nextCategoryId = db['categories'][categoryIndex + 1]?.title;
+        if (nextCategoryId) {
+          window.location.href = window.location.origin + '/' + nextCategoryId;
+        }
+      }
+
+      if (touchendX - touchstartX > 50) {
+        const categoryIndex = db['categories'].indexOf(category);
+        const lastCategoryId = db['categories'][categoryIndex - 1]?.title;
+        if (lastCategoryId) {
+          window.location.href = window.location.origin + '/' + lastCategoryId;
+        }
+      }
+    }
+
+    return (
+      <>
+        <div className="page song-page">
+          <p className="category-header">
+            {category.id}. {category.title}
+            {category.title.includes('40') && ' %'}
+          </p>
+          <div className="category-image-container">
+            <img
+              src={require('../../images/categories/' + category.image + '.png')}
+              alt={category.image}
+              height="100%"
+            />
+          </div>
+          <MenuSongs category={category.title} songs={this.props.songs} />
+        </div>
+        <div className="song-footer">
+          <a href={category.title} className="song-footer-category">
+            {category.title}
+            {category.title.includes('40') && ' %'}
+          </a>
+        </div>
+      </>
+    );
+  }
 }
 
-export default Song;
+export default Category;
