ab428c65b972367125f6f945c16b3ece43c789a1
Petro Silenius <petro.silenius@gmail.com>
2021-09-26 20:10:05 +0300

    Update to react-17, react-scripts v4 and react-select v5

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import MenuButton from './MenuButton';
 import Menu from './Menu';
 import Search from './Search';
+import logo from '../images/topbar.png';
 
 
 class TopBar extends Component {
@@ -30,7 +31,7 @@ class TopBar extends Component {
 			<div id="top-bar" className="top-bar">
 				<a href="/" className="top-bar-heading">
 					<img
-						src={require('../images/topbar.png')}
+						src={logo}
 						alt="Urho Matti 3.0"
 						height="29px"
 					/>
--- src/views/Landing/LandingPage.jsx
+++ src/views/Landing/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from '../../images/landing.svg'
 
 class LandingPage extends Component {
   render() {
@@ -13,7 +14,7 @@ class LandingPage extends Component {
         <div className="landing-image-container">
           <img
             className="landing-image"
-            src={require('../../images/landing.svg')}
+            src={logo}
             alt="Urho Matti 3.0 logo"
             height="100%"
             width="100%"
